53960f87549d27d9a2383e6500e3478d912e329a
pscore23 <rubik2357x@gmail.com>
2023-02-04 13:48:09 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+class StdoutColors {
+  static final dynamic Function(dynamic) _selectColor =
+      (col) => (text) => '\u001b[${col}m${text}\u001b[0m';
+
+  static final dynamic black = _selectColor('30');
+  static final dynamic red = _selectColor('31');
+  static final dynamic green = _selectColor('32');
+  static final dynamic yellow = _selectColor('33');
+  static final dynamic blue = _selectColor('34');
+  static final dynamic magenta = _selectColor('35');
+  static final dynamic cyan = _selectColor('36');
+  static final dynamic white = _selectColor('37');
+}
+
+void main(List<String> args) {
+  print('[${StdoutColors.black('testing')}]');
+}
